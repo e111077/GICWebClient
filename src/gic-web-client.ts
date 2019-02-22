25d073f01c27a07015089643a783aeefab38f068
Elliott Marquez <me@elliottmarquez.com>
2019-02-22 00:08:14 -0800

    configure first webcomponent

---
+++ src/gic-web-client.ts
@@ -0,0 +1,8 @@
+import {LitElement, customElement, html} from 'lit-element';
+
+@customElement('gic-web-client')
+class GicWebClient extends LitElement {
+  render() {
+    return html`<div>Hello world</div>`
+  }
+}
